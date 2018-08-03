7582ada4974291db727ccf8dcc132240c1bf958f
venus36 <venus0036@mail.com>
2018-08-03 18:53:32 +0800

    remove array

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,24 +1,12 @@
 const Expense = require('../models/Expense');
 
 exports.index = (req, res) => {
-  const expenses = [];
-
   Expense.find({}, (err, expenseItems) => {
     if (err) throw err;
 
-    expenseItems.forEach((expenseItem) => {
-      const expense = {
-        date: expenseItem.date,
-        time: expenseItem.time,
-        description: expenseItem.description,
-        amount: expenseItem.amount,
-        comment: expenseItem.comment,
-      };
-      expenses.push(expense);
-    });
     res.render('expenses/view', {
       title: 'Track Expenses',
-      expenseList: expenses
+      expenseList: expenseItems
     });
   });
 };
